ea823b13b9a1080eee3efde72d79216c5a0affd0
Carlo Aironi <aircarlo@gmail.com>
2025-11-21 12:25:59 +0100

    Refactor loss classes for stability and clarity
    Updated loss classes to improve numerical stability and renamed LogSpectralConvLoss to SpectralConvLoss.

--- loss.py
+++ loss.py
@@ -2,25 +2,29 @@ import torch
 import torch.nn as nn
 import torchaudio
 
-class LogSpectralConvLoss(nn.Module):
+class SpectralConvLoss(nn.Module):
     """
-    log-Spectral convergence loss module.
+    Spectral convergence loss module.
+    see https://arxiv.org/pdf/1808.06719.pdf
     """
 
     def __init__(self):
-        super(LogSpectralConvLoss, self).__init__()
+        super(SpectralConvLoss, self).__init__()
 
     def forward(self, x_mag, y_mag):
         return torch.norm((y_mag - x_mag), p="fro") / torch.norm(y_mag, p="fro")
 
 
-class LogSTFTMagnitudeLoss(nn.Module):
+class LogSTFTMagnitudeLoss(torch.nn.Module):
     """
     Log STFT magnitude loss module.
+    see https://arxiv.org/pdf/1808.06719.pdf
     """
 
     def __init__(self):
         super(LogSTFTMagnitudeLoss, self).__init__()
 
     def forward(self, x_mag, y_mag):
-        return nn.functional.l1_loss(torch.log(y_mag+1e-8), torch.log(x_mag+1e-8))
+        x_mag = torch.clamp(x_mag, min=1e-6, max=1e3) # avoid underflows & overflows
+        y_mag = torch.clamp(y_mag, min=1e-6, max=1e3)
+        return F.l1_loss(torch.log(x_mag), torch.log(y_mag))
